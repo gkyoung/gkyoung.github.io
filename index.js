108abda797fc5ba6b6086c68097e1816f25cb4a9
gkyoung <gkyoung01@gmail.com>
2018-06-10 15:43:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 init();
 
 //load the model: the first parameter is the url to the model, the second is the scale of the model (it works only with gltf filese)
-loadModel('./busterDrone/tester.gltf',1);
+loadModel('./busterDrone/try.gltf',1);
 //loadModel('https://raw.githubusercontent.com/u4bi-git/arjs-3dmodel-loader-test/master/assets/model/knight/knight.js');
 
